03086ec83ec7d8872ae9c4e411c578db011d8214
Kaheem Freeman <kfreeman@Kaheems-MacBook-Pro.local>
2024-11-30 02:00:17 -0500

    Remove old auth.ts and update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,15 @@
-export { auth as middleware } from "./auth"
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname.startsWith("/")) {
+        return token !== null;
+      }
+      return true;
+    },
+  },
+})
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
